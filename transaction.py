b271a7fd93793fbd663b438a22139130b6cf1ff3
Kevin Rosengren <rosenkev4@gmail.com>
2025-08-06 03:39:15 -0500

    Created class for Transaction

---
+++ transaction.py
@@ -0,0 +1,15 @@
+class Transaction:
+
+    def __init__(self, date, time, description, vendor, amount):
+        self.date = date
+        self.time = time
+        self.description = description
+        self.vendor = vendor
+        self.amount = amount
+    
+    def print_data(self):
+        print('Date:', self.date)
+        print('Time:', self.time)
+        print('Description:', self.description)
+        print('Vendor', self.vendor)
+        print(f'Amount ${self.amount}:,.2f')
