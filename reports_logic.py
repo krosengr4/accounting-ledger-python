726291553e073b47a89e4bdcba734341cbdd5b63
Kevin Rosengren <rosenkev4@gmail.com>
2025-08-12 20:57:00 -0500

    Created skeleton methods to display transactions for this month, last month, this year, and last year

--- reports_logic.py
+++ reports_logic.py
@@ -1,2 +1,14 @@
 def process_reports_screen():
     print('Reports screen')
+
+def trans_this_month():
+    print('Transactions this month')
+
+def trans_last_month():
+    print('Transactions last month')
+
+def trans_this_year():
+    print('Transactions this year')
+
+def trans_last_year():
+    print('Transactions last year')
