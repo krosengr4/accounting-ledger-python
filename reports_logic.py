6a21e853f47331a4a40a71089965ede635dad053
Kevin Rosengren <rosenkev4@gmail.com>
2025-08-12 21:01:35 -0500

    Entered logic method to process user choice from the reports screen

--- reports_logic.py
+++ reports_logic.py
@@ -1,5 +1,25 @@
+import user_interface, file_manager
+from transaction import Transaction
+
 def process_reports_screen():
-    print('Reports screen')
+    if_continue = True
+
+    while(if_continue):
+        user_choice = user_interface.display_reports_screen()
+
+        match user_choice:
+            case 1:
+                trans_this_month()
+            case 2:
+                trans_last_month()
+            case 3:
+                trans_this_year()
+            case 4:
+                trans_last_year()
+            case 0:
+                if_continue = False
+            case _:
+                print("Please enter a valid option!!!")           
 
 def trans_this_month():
     print('Transactions this month')
